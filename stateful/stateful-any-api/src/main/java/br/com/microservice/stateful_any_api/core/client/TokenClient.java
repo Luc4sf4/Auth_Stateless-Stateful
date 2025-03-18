46d37948854c7617195d4f0449f0200349aec644
Luc4sf4 <superlucas2005@gmail.com>
2025-03-18 11:12:29 -0300

    Any-api: Criando o client HTTP com AUTH-API

--- stateful/stateful-any-api/src/main/java/br/com/microservice/stateful_any_api/core/client/TokenClient.java
+++ stateful/stateful-any-api/src/main/java/br/com/microservice/stateful_any_api/core/client/TokenClient.java
@@ -1,4 +1,19 @@
 package br.com.microservice.stateful_any_api.core.client;
 
+import br.com.microservice.stateful_any_api.core.dto.AuthUserResponse;
+import br.com.microservice.stateful_any_api.core.dto.TokenDTO;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+@HttpExchange("/pi/auth")//sempre chama o localhost:8082
 public interface TokenClient {
+
+    @PostExchange("/token/validate")
+    TokenDTO validateToken(@RequestHeader String accessToken);
+
+    @GetExchange("/user")
+    AuthUserResponse getAuthenticateUser(@RequestHeader String accessToken);
+
 }
